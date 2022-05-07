b1171928cf7a30bf5a2863b11da3368817915179
Henrique Rubido <henriquerubido1@gmail.com>
2022-05-07 20:07:46 -0300

    day 1 part 2

--- day-1/index.ts
+++ day-1/index.ts
@@ -1,7 +1,11 @@
-function sum(num1:number, num2: number) {
-    console.log(num1);
-    
-    return num1 + num2;
-}
+const button = document.querySelector("button");
+const input1 = document.getElementById("num1")! as HTMLInputElement;
+const input2 = document.getElementById("num2")! as HTMLInputElement;
 
-sum(1, 2);
+function add(num1: number, num2: number) {
+    return num1 + num2;
+};
+
+button.addEventListener("click", function() {
+    console.log(add(+input1.value, +input2.value));
+});
