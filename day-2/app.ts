893be528c54e9b0c8226cb24dfc818f045a789de
Henrique Rubido <henriquerubido1@gmail.com>
2022-05-08 13:27:19 -0300

    day 2 part 3

--- day-2/app.ts
+++ day-2/app.ts
@@ -1,11 +1,23 @@
 // const person: {
-// 	name: string,
-// 	age: number
+// 	name: string;
+// 	age: number;
+// 	hobbies: string[];
+// 	// role is a tuple, which is a variable type that is
+// 	// is similar to an array, but with pre-determined 
+// 	// length and value types
+// 	role: [number, string];
 // } = {
+// 	name: 'henrique',
+// 	age: 20,
+// 	hobbies: ['Sports', 'Cooking'],
+// 	role: [2, 'author']
+// };
+
 const person = {
 	name: 'henrique',
 	age: 20,
-	hobbies: ['Sports', 'Cooking']
+	hobbies: ['Sports', 'Cooking'],
+	role: [2, 'author']
 };
 
 // sets an array of strings
