b4f97fc77a5f6f16634d691613d6f00ff2492c31
Henrique Rubido <henriquerubido1@gmail.com>
2022-05-08 19:12:23 -0300

    day 2 part 7

--- day-2/app.ts
+++ day-2/app.ts
@@ -1,17 +1,37 @@
-// union type example bellow
+// union and literal type examples bellow
+// union type accepts more than one type of variable
+// input1, input2 and resultConversion are union type variables
 
-function combine(input1: number | string, input2: number | string) {
+// literal type accepts only pre-determined values
+// resultConversion is a literal type variable
+
+
+// type alias is a way to create types in TypeScript
+type Combinable = number | string;
+type Literal = 'as-number' | 'as-text';
+
+function combine(
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: Literal
+) {
   let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number') {
-    result = input1 + input2;
+  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+    result = +input1 + +input2;
   } {
     result = input1.toString() + input2.toString();
   }
-  return result;
+
+	if (resultConversion === 'as-number') {
+		return +result;
+	} {
+		return result.toString();
+	}
 }
 
-const combinedAges = combine(30, 26);
-const combinedNames = combine('Max', 'Anna');
+const combinedAges = combine(30, 26, 'as-number');
+const combinedStringAges = combine('30', '26', 'as-number');
+const combinedNames = combine('Max', 'Anna', 'as-text');
 console.log(combinedAges);
 console.log(combinedNames);
 
