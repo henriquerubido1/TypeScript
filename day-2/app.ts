1d9f30bc5de13f30edb72a478fb217b87c07e8b8
Henrique Rubido <henriquerubido1@gmail.com>
2022-05-08 13:36:25 -0300

    day 2 part 4

--- day-2/app.ts
+++ day-2/app.ts
@@ -13,11 +13,24 @@
 // 	role: [2, 'author']
 // };
 
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+// enum is a variable type which creates a sort of object
+// in which the keys are values that are incremented by one
+
+enum Role { ADMIN, READ_ONLY, AUTHOR };
+enum Test { TEST1 = 7, TEST2, TEST3 };
+enum Test2 { STR1 = 'STR1', STR2 = 0, STR3 }
+console.log(Test.TEST2);
+
+
 const person = {
 	name: 'henrique',
 	age: 20,
 	hobbies: ['Sports', 'Cooking'],
-	role: [2, 'author']
+	role: Role.ADMIN
 };
 
 // sets an array of strings
