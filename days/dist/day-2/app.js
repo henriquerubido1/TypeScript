5b6b662b1454c369e1c1b7b7ef88802d2da1c602
Henrique Rubido <henriquerubido1@gmail.com>
2022-05-11 15:42:45 -0300

    removeComments

---
+++ days/dist/day-2/app.js
@@ -0,0 +1,17 @@
+"use strict";
+// an unknwon variable is a bit more precise
+// than an any variable 
+let userInput;
+let userName;
+userInput = 5;
+userInput = 'Henri';
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+// never is different from void, since never 
+// specifies that the function returns nothing
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError('An error occured!', 500);
+//# sourceMappingURL=app.js.map
---
+++ days/dist/day-3/app.js
@@ -0,0 +1,6 @@
+"use strict";
+const btn = document.querySelector('button');
+button === null || button === void 0 ? void 0 : button.addEventListener('click', () => {
+    console.log('clicked');
+});
+//# sourceMappingURL=app.js.map
