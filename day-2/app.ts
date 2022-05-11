05e4fbc5a9c1c46372be7e2e7409574cfeb21c18
Henrique Rubido <henriquerubido1@gmail.com>
2022-05-11 13:44:47 -0300

    day 2 part 10

--- day-2/app.ts
+++ day-2/app.ts
@@ -10,3 +10,12 @@ userInput = 'Henri';
 if (typeof userInput === 'string') {
   userName = userInput;
 }
+
+// never is different from void, since never 
+// specifies that the function returns nothing
+
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code }
+}
+
+generateError('An error occured!', 500);
